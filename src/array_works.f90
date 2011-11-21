12995a0de016e6579335a9b62802cd37526270f3
minzastro <minzastro@localhost>
2011-11-21 10:30:37 +0100

    Load from file fast

--- src/array_works.f90
+++ src/array_works.f90
@@ -14,23 +14,15 @@ integer, save :: iIgnoranceMode = 1
 integer, save :: iSkipAmount = 0 ! Number of lines to be skipped from the beginning of the data (header size)
 
 contains
-! Subroutine to load data from text file into double-precision array.
-! Number of lines/columns in file is detected,
-! Comments are also supported as lines, starting with 'cComment' character (default to ';')
-subroutine LoadFromFile(sFilename, datatable, iColNum, iSize) !Load data from ASCII file
-!                               Input parameters
+
+integer function open_file(sFilename)
+! Tests file existence and opens it.
+! Returns file unit.
+! Sets unit to std-input when sFilename is empty.
 character(*), intent(in) :: sFilename
-!                               Output parameters
-real*8, intent(out) :: datatable(MAX_ROW, 0:MAX_COLUMN) !Data from file will be placed here
-integer, intent(out) :: iColNum !Number of columns in file
-integer, intent(out) :: iSize   !Number of lines in file
 
 integer unit_id
-logical file_exists, flag
-integer i, istat, iTemp, iLine
-character*(LINE_LENGTH) sLine                    !! one line from file
-character*(512) sErrorMsg
-  datatable(:,:)=0D0
+logical file_exists
 
   if (trim(sFilename).eq.'') then
     if (verbose) then
@@ -48,58 +40,80 @@ character*(512) sErrorMsg
       stop
     endif
   endif
+  open_file = unit_id
+end function open_file
 
-	!loading data from file======================================
-	i = 0
-	istat = 0
-	!detect number of columns
-	flag = .false.
-	iLine = 0
-	!Skipping header
-	do i = 1, iSkipAmount
-	  read(unit_id,'(a)', iostat=istat) sLine
-	  if (istat.ne.0) then
-		write(*,*) 'Error! No data to proceed!'
-		stop
-	  endif
-	enddo
-	! Reading the data
-	do while (flag.eqv..false.)
-	  read(unit_id,'(a)', iostat=istat) sLine
-	  if (istat.ne.0) then
-		write(*,*) 'Error! No data to proceed!'
-		stop
-	  endif
-	  iLine = iLine + 1
-	  if (sLine(1:1) .ne. cComment) then !Looking for first non-commented line
-		call replace_substring(sLine, '	', ' ') !replacing TABS with SPACEs
-		call TrimLeft(sLine, sLine) !adjust left
-		call sqeeze(sLine,' ') !remove double SPACEs
-		if (len(trim(sLine)).gt.0) then
-		  iColnum=1 !first column
-		endif
-		do i = 1, len(trim(sLine)) !counting remaining spaces
-		  if (sLine(i:i).eq.' ') then
-			iColnum=iColnum+1
-		  endif
-		enddo
-		flag = .true.
-	  endif
-	enddo
-	i = 1
-	if (verbose) then
-	  write(*,*) cComment,iColnum, ' columns detected  from line ', iLine
-	endif
-	!Checking iColnum
-	if (iColnum.gt.MAX_COLUMN) then
-	  write(sErrorMsg, *) cComment//' Warning! Too many columns in file, reducing to max. allowed:', MAX_COLUMN
-	  write(*,*) sErrorMsg
-	  iColnum = MAX_COLUMN
-	endif
-	!Reading data
-	!TODO: add checks for non-numeric data in file, replace/store elsewhere    
-	do while ((istat.eq.0).and.(i.le.MAX_ROW))
-	  if ((sLine(1:1).ne.cComment).and.(len(trim(sLine)).gt.0)) then
+
+! Subroutine to load data from text file into double-precision array.
+! Number of lines/columns in file is detected,
+! Comments are also supported as lines, starting with 'cComment' character (default to ';')
+subroutine LoadFromFile(sFilename, datatable, iColNum, iSize) !Load data from ASCII file
+!                               Input parameters
+character(*), intent(in) :: sFilename
+!                               Output parameters
+real*8, intent(out) :: datatable(MAX_ROW, 0:MAX_COLUMN) !Data from file will be placed here
+integer, intent(out) :: iColNum !Number of columns in file
+integer, intent(out) :: iSize   !Number of lines in file
+
+integer unit_id
+logical flag
+integer i, istat, iTemp, iLine
+character*(LINE_LENGTH) sLine                    !! one line from file
+character*(512) sErrorMsg
+  datatable(:,:)=0D0
+  unit_id = open_file(sFilename)
+
+  !loading data from file======================================
+  i = 0
+  istat = 0
+  !detect number of columns
+  flag = .false.
+  iLine = 0
+  !Skipping header
+  do i = 1, iSkipAmount
+    read(unit_id,'(a)', iostat=istat) sLine
+    if (istat.ne.0) then
+    write(*,*) 'Error! No data to proceed!'
+    stop
+    endif
+  enddo
+  ! Reading the data
+  do while (flag.eqv..false.)
+    read(unit_id,'(a)', iostat=istat) sLine
+    if (istat.ne.0) then
+    write(*,*) 'Error! No data to proceed!'
+    stop
+    endif
+    iLine = iLine + 1
+    if (sLine(1:1) .ne. cComment) then !Looking for first non-commented line
+    call replace_substring(sLine, '  ', ' ') !replacing TABS with SPACEs
+    call TrimLeft(sLine, sLine) !adjust left
+    call sqeeze(sLine,' ') !remove double SPACEs
+    if (len(trim(sLine)).gt.0) then
+      iColnum=1 !first column
+    endif
+    do i = 1, len(trim(sLine)) !counting remaining spaces
+      if (sLine(i:i).eq.' ') then
+      iColnum=iColnum+1
+      endif
+    enddo
+    flag = .true.
+    endif
+  enddo
+  i = 1
+  if (verbose) then
+    write(*,*) cComment,iColnum, ' columns detected  from line ', iLine
+  endif
+  !Checking iColnum
+  if (iColnum.gt.MAX_COLUMN) then
+    write(sErrorMsg, *) cComment//' Warning! Too many columns in file, reducing to max. allowed:', MAX_COLUMN
+    write(*,*) sErrorMsg
+    iColnum = MAX_COLUMN
+  endif
+  !Reading data
+  !TODO: add checks for non-numeric data in file, replace/store elsewhere    
+  do while ((istat.eq.0).and.(i.le.MAX_ROW))
+    if ((sLine(1:1).ne.cComment).and.(len(trim(sLine)).gt.0)) then
       read(sLine, fmt=*, iostat=iTemp) datatable(i, 1:iColnum)
       if (iTemp.gt.0) then
         if (verbose) then
@@ -109,16 +123,50 @@ character*(512) sErrorMsg
         datatable(i, 0) = dble(i)
         i = i + 1
       endif
-	  endif
-	  read(unit_id, '(a)', iostat=istat) sLine
-	  iLine = iLine + 1
-	enddo
+    endif
+    read(unit_id, '(a)', iostat=istat) sLine
+    iLine = iLine + 1
+  enddo
   if (unit_id.ne.5) then
     close(unit_id)
   endif
   iSize = i - 1
 end subroutine LoadFromFile
 
+subroutine LoadFromFileFast(sFilename, iColnum, datatable, iSize)
+!Load data from ASCII file with known number of columns
+!                               Input parameters
+character(*), intent(in) :: sFilename
+integer, intent(in) :: iColNum !Number of columns in file
+!                               Output parameters
+real*8, intent(out) :: datatable(MAX_ROW, 0:MAX_COLUMN) !Data from file will be placed here
+integer, intent(out) :: iSize   !Number of lines in file
+
+integer unit_id
+logical flag
+integer i, istat, iTemp, iLine
+character*(LINE_LENGTH) sLine                    !! one line from file
+character*(512) sErrorMsg
+
+  datatable(:,:)=0D0
+
+  unit_id = open_file(sFilename)
+  !loading data from file======================================
+  i = 0
+  istat = 0
+  !detect number of columns
+  flag = .false.
+  iLine = 0
+  !Skipping header
+  do i = 1, iSkipAmount
+    read(unit_id,'(a)', iostat=istat) sLine
+    if (istat.ne.0) then
+    write(*,*) 'Error! No data to proceed!'
+    stop
+    endif
+  enddo
+
+end subroutine LoadFromFileFast
 
 subroutine LoadFromFileExt(sFilename, datatable, iColNum, iSize)
 !Load data from ASCII file with possible character fields
@@ -130,67 +178,52 @@ integer, intent(out) :: iColNum !Number of columns in file
 integer, intent(out) :: iSize   !Number of lines in file
 
 integer unit_id
-logical file_exists, flag
+logical flag
 integer i, istat, iTemp, iLine
 character*(LINE_LENGTH) sLine  !! one line from file
 type(TStringArray) :: aFields
 character*(512) sErrorMsg
   datatable(:,:)=0D0
 
-  if (trim(sFilename).eq.'') then
-    if (verbose) then
-      write(*,*) cComment//' No filename specified, proceeding with STDIN'
+  unit_id = open_file(sFilename)
+
+  !loading data from file======================================
+  i = 0
+  istat = 0
+  !detect number of columns
+  flag = .false.
+  iLine = 0
+  !Skipping header
+  do i = 1, iSkipAmount
+    read(unit_id,'(a)', iostat=istat) sLine
+    if (istat.ne.0) then
+    write(*,*) 'Error! No data to proceed!'
+    stop
     endif
-    unit_id = 5
-  else
-    !checking for input file existance
-    inquire(file=trim(sFilename), exist=file_exists)
-    if (file_exists) then
-      unit_id = find_io(7)
-      open(unit=unit_id, file=trim(sFilename), status='UNKNOWN')
-    else
-      write(*, '(a)') 'Error! File "'//trim(sFilename)//'" does not exists! '
+  enddo
+  do while (flag.eqv..false.)
+    read(unit_id, '(a)', iostat=istat) sLine
+    iLine = iLine + 1
+    if (istat.ne.0) then
+      write(*,*) 'Error! No data to proceed!'
       stop
     endif
-  endif
-
-	!loading data from file======================================
-	i = 0
-	istat = 0
-	!detect number of columns
-	flag = .false.
-	iLine = 0
-	!Skipping header
-	do i = 1, iSkipAmount
-	  read(unit_id,'(a)', iostat=istat) sLine
-	  if (istat.ne.0) then
-		write(*,*) 'Error! No data to proceed!'
-		stop
-	  endif
-	enddo
-  do while (flag.eqv..false.)
-	  read(unit_id, '(a)', iostat=istat) sLine
-	  iLine = iLine + 1
-	  if (istat.ne.0) then
-  		write(*,*) 'Error! No data to proceed!'
-	  	stop
-	  endif
-	  if (sLine(1:1) .ne. cComment) then !Looking for first non-commented line
-	    call RemoveDelimiters(sLine, bRemoveDuplicateDelimiters, bTabsToSpaces)
-		  if (cDelimiter.eq.' ') then
-		    call TrimLeft(sLine, sLine)
-		  endif 
-			if (len(trim(sLine)).gt.0) then
-			  iColnum = 1 !first column
-			endif
-			do i = 1, len(trim(sLine)) !counting remaining spaces
-			  if (sLine(i:i).eq.cDelimiter) then
-				  iColnum = iColnum + 1
-			  endif
-			enddo
-			flag = .true.
-	  endif
-	enddo
+    if (sLine(1:1) .ne. cComment) then !Looking for first non-commented line
+      call RemoveDelimiters(sLine, bRemoveDuplicateDelimiters, bTabsToSpaces)
+      if (cDelimiter.eq.' ') then
+        call TrimLeft(sLine, sLine)
+      endif 
+      if (len(trim(sLine)).gt.0) then
+        iColnum = 1 !first column
+      endif
+      do i = 1, len(trim(sLine)) !counting remaining spaces
+        if (sLine(i:i).eq.cDelimiter) then
+          iColnum = iColnum + 1
+        endif
+      enddo
+      flag = .true.
+    endif
+  enddo
   istat = 0
   iLine = 0
   do while ((istat .eq. 0).and.(i.le.MAX_ROW))
@@ -227,22 +260,22 @@ character*(*), intent(inout) :: sLine
 logical, intent(in) :: bRemoveRepeat
 logical, intent(in) :: bExpandTabs
 integer ipos, ilength
-	if (bExpandTabs) then !replacing TABS with SPACEs
-  	call replace_substring(sLine, char(9),' ') 
-	endif
-	if (bRemoveRepeat) then !removing duplicate delimiters
-	  ipos = 1
-	  ilength = len(trim(sLine))
-	  do while (ipos.le.ilength-1)
-	    if ((sLine(ipos:ipos).eq.cDelimiter).and. &
-	        (sLine(ipos+1:ipos+1).eq.cDelimiter)) then
-	      sLine(ipos:ilength) = sLine(ipos+1:ilength+1)
-	      ilength = ilength - 1
-	    else
-	      ipos = ipos + 1
-	    endif
-	  enddo
-	endif
+  if (bExpandTabs) then !replacing TABS with SPACEs
+    call replace_substring(sLine, char(9),' ') 
+  endif
+  if (bRemoveRepeat) then !removing duplicate delimiters
+    ipos = 1
+    ilength = len(trim(sLine))
+    do while (ipos.le.ilength-1)
+      if ((sLine(ipos:ipos).eq.cDelimiter).and. &
+          (sLine(ipos+1:ipos+1).eq.cDelimiter)) then
+        sLine(ipos:ilength) = sLine(ipos+1:ilength+1)
+        ilength = ilength - 1
+      else
+        ipos = ipos + 1
+      endif
+    enddo
+  endif
 end subroutine RemoveDelimiters
 
 !! Subroutine to create an array, containing number sequence (like 0,1,2,3,.. etc.)
@@ -276,27 +309,12 @@ integer, intent(out) :: iColNum !Number of columns in file
 integer, intent(out) :: iSize   !Number of lines in file
 
 integer unit_id
-logical file_exists, flag
+logical flag
 integer i, istat, iTemp, iLine, iColTotalEstimate
 character*(LINE_LENGTH) sLine   !! one line from file
 character*(512) sErrorMsg
 character*(50) sField
-  if (trim(sFilename).eq.'') then
-    if (verbose) then
-      write(*,*) cComment//' No filename specified, proceeding with STDIN'
-    endif
-    unit_id = 5
-  else
-    !checking for input file existance
-    inquire(file=trim(sFilename), exist=file_exists)
-    if (file_exists) then
-      unit_id=find_io(7)
-      open(unit=unit_id, file=trim(sFilename), status="UNKNOWN")
-    else
-      print *, 'Error! File "', trim(sFilename), '" does not exists! '
-      stop
-    endif
-  endif
+  unit_id = open_file(sFilename)
   !loading data from file======================================
   iLine = 0
   istat = 0
@@ -306,14 +324,14 @@ character*(50) sField
   datatable(:,:) = 0D0
   iFieldBreaks(1) = 1
   iColTotalEstimate = 1
-	!Skipping header
-	do i = 1, iSkipAmount
-	  read(unit_id,'(a)', iostat=istat) sLine
-	  if (istat.ne.0) then
-		write(*,*) 'Error! No data to proceed!'
-		stop
-	  endif
-	enddo
+  !Skipping header
+  do i = 1, iSkipAmount
+    read(unit_id,'(a)', iostat=istat) sLine
+    if (istat.ne.0) then
+    write(*,*) 'Error! No data to proceed!'
+    stop
+    endif
+  enddo
   !Parsing first non-comment line - should contain field separators
   do while (istat.eq.0)
     read(unit_id, '(a)', iostat=istat) sLine
